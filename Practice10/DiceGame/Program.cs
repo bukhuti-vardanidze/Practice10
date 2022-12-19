69c8a80e360e65571af24fc60ed92edfe93d76bc
bukhuti-vardanidze <84092360+bukhuti-vardanidze@users.noreply.github.com>
2022-12-19 23:32:30 +0400

    Practice

---
+++ Practice10/DiceGame/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace DiceGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter Your Name : ");
+            var firstName = Console.ReadLine();
+            Console.WriteLine($"Hello, {firstName}!");
+            Console.WriteLine("Game is starting...");
+
+            int player1Random, player2Random;
+            int player1point = 0;
+             int   player2point = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Press Any Key to Start Game!");
+                Console.ReadLine();
+
+                Random random = new Random();
+               
+                player1Random= random.Next(1, 6);
+                Console.WriteLine($"{firstName} your dice number is : {player1Random} ");
+               
+
+
+                player2Random = random.Next(1, 6);
+                Console.WriteLine($"AI  dice number is : {player2Random} ");
+               
+
+
+                if (player1Random == player2Random)
+                {
+                    Console.WriteLine("The game ended in a draw !");
+                    Console.WriteLine();
+
+                }
+                else if(player1Random >= player2Random)
+                {
+                    Console.WriteLine($" {firstName}, You Won !");
+                    player1point++;
+                    Console.WriteLine($"{firstName}, your points are - {player1point}");
+                    Console.WriteLine($"Ai points are - {player2point}");
+
+
+
+                }
+                else if (player1Random <= player2Random)
+                {
+                    
+                    Console.WriteLine(" AI Won !");
+                    player2point++;
+                    Console.WriteLine($"{firstName}, your points are - {player1point}");
+                    Console.WriteLine($"Ai points are - {player2point}");
+
+                }
+               
+             
+
+            }
+
+        }
+    }
+}
---
+++ Practice10/ParcticeString/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+
+namespace PracticeString
+{
+    public class StringMethods
+    {
+        public static void ReverseString()
+        {
+            string str = "w3resource.com";
+            int l = 0;
+            Console.Write("");
+            while (l <= str.Length - 1)
+            {
+                Console.WriteLine(str[l]);
+                l++;
+            }
+            Console.WriteLine();
+            Console.WriteLine("*************************");
+            Console.WriteLine();
+        }
+
+
+    }
+
+    class Program
+    {
+       public static void Main()
+        {
+            StringMethods.ReverseString();
+
+          
+        }
+    }
+}
